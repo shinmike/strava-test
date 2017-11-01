12ddfb01a54cf8e962bf4600e59be8ad9690da21
shinmike <>
2017-11-01 11:58:02 -0700

    first and last name, obtained through strava, showing in logbook

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const strava = require('strava-v3');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 // strava token info
 const STRAVA_CLIENT_ID = process.env.STRAVA_CLIENT_ID;
@@ -11,6 +13,17 @@ const STRAVA_REDIRECT_URI = process.env.STRAVA_REDIRECT_URI;
 const STRAVA_REQUEST_ACCESS_URL = `https://www.strava.com/oauth/authorize?client_id=${STRAVA_CLIENT_ID}&redirect_uri=${STRAVA_REDIRECT_URI}&response_type=code&scope=write`;
 
 const app = express();
+
+//Session cookies
+app.use(session({
+	secret: "MajesticSeaFlapFlap",
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		secure: false,
+		maxAge: 432000000 //5 days in milliseconds
+	},
+}));
 
 const PORT = process.env.PORT || '4000';
 
@@ -22,27 +35,33 @@ app.get('/', (req, res) => {
 	if (login_url === undefined || login_url === "") {
 		throw new Error("Unable to retrieve Strava login URL");
 	}
-  res.render("landing-page", { strava_request_access_url: STRAVA_REQUEST_ACCESS_URL });
+	res.render("landing-page", { strava_request_access_url: STRAVA_REQUEST_ACCESS_URL });
 });
 
-app.get('/tokenexchange', function (req, res) {
-  //Get the access token, save it in session variable, and redirect to training log
-  var code = req.query.code;
+app.get('/tokenexchange', (req, res) => {
+	//Get the access token, save it in session variable, and redirect to training log
+	var code = req.query.code;
 	strava.oauth.getToken(code, function (err, payload, limits) {
 		console.log("Token exchange payload = " + JSON.stringify(payload));
 		if (err) {
 			throw new Error("Unable to request access token");
-    }
-		// req.session.strava_token = payload.access_token;
+		}
+		req.session.strava_token = payload.access_token;
 		res.redirect('/logbook');
 	});
 });
 
-app.get('/logbook', function (req, res) {
-	res.render('logbook', {});
+app.get('/logbook', (req, res) => {
+	var code = req.query.code;
+	strava.athlete.get({ 'access_token': code }, function (err, payload, limits) {
+		var athlete = {};
+		athlete.firstname = payload.firstname;
+		athlete.lastname = payload.lastname;
+		res.render('logbook', {athleteInfo: athlete});
+	});
 });
 
 // -------------------------------------------------- Initialize app
 app.listen(PORT, () => {
-  console.log(`app listening on port ${PORT}!`);
+	console.log(`app listening on port ${PORT}!`);
 });
